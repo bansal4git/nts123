f70fd2d02fc39d3df484c17cf219d8df4b57ffc3
Ajay Bansal <ajju.bansal@gmail.com>
2024-07-26 10:07:16 +0000

    c1Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws vpc" test1 {
+resource "aws vpc" "test1" {
 cidr_block = "192.168.1.1/16"
 tags = {
 "Name" = "VPC1"
 
 }
 }
-resource "aws vpc" test2 {
+resource "aws vpc" "test2" {
 cidr_block = "192.168.2.1/16"
 tags = {
 "Name" = "VPC2"
